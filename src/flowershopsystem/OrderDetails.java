12c3272026be9b39514a2e97acbb3e7480a3c54a
LIANG <LIANG@DESKTOP-BD76IC7>
2018-11-20 23:35:40 +0800

    Part C

---
+++ src/flowershopsystem/OrderDetails.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flowershopsystem;
+
+import java.util.Date;
+
+/**
+ *
+ * @author LIANG
+ */
+public class OrderDetails {
+    String pickupdate;
+    String time;
+    String address;
+    String payment;
+    
+    public OrderDetails(String pickupdate, String time, String address, String payment){
+    this.pickupdate = pickupdate;
+    this.time = time;
+    this.address = address;
+    this.payment = payment;
+          
+    }
+
+    public String getPickupdate() {
+        return pickupdate;
+    }
+
+    public void setPickupdate(String pickupdate) {
+        this.pickupdate = pickupdate;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetails{" + "pickupdate=" + pickupdate + ", time=" + time + ", address=" + address + ", payment=" + payment + '}';
+    }
+    
+
+}
